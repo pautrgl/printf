3ce42c6bfe53ee3de8803d761bfde378e2202902
pautrgl <paulatrgl@gmail.com>
2024-10-22 00:48:15 +0200

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ptrapero <ptrapero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 18:12:58 by ptrapero          #+#    #+#             */
-/*   Updated: 2024/10/21 22:07:41 by ptrapero         ###   ########.fr       */
+/*   Updated: 2024/10/21 23:17:47 by ptrapero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 # include <stdarg.h>
 # include "libft/libft.h"
 
-int		ft_printf(char const *data, ...);
-int		ft_print_p(void *p, int j);
-int		ft_print_u(unsigned int n, int j);
-int		ft_print_x(unsigned long long n, int j);
-int		ft_print_super_x(unsigned int n, int j);
+int	ft_printf(char const *data, ...);
+int	ft_print_p(void *p, int j);
+int	ft_print_u(unsigned int n, int j);
+int	ft_print_x(unsigned long long n, int j);
+int	ft_print_super_x(unsigned int n, int j);
 
 #endif
--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ptrapero <ptrapero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 22:12:10 by ptrapero          #+#    #+#             */
-/*   Updated: 2024/10/21 19:07:53 by ptrapero         ###   ########.fr       */
+/*   Updated: 2024/10/22 00:06:36 by ptrapero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@ int	ft_putstr_fd(char *s, int fd, int j)
 {
 	if (s == NULL)
 	{
-		write(1, "(null)", 6);
-		j += 6;
+		j += write(1, "(null)", 6);
 		return (j);
 	}
-	write(fd, s, ft_strlen(s));
-	j += ft_strlen(s);
+	j += write(fd, s, ft_strlen(s));
 	return (j);
 }
 
